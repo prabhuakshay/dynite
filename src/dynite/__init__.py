2bcab2f363be54d20874090d167d9e6005874be7
prabhuakshay <akshay.prabhu.mulki@gmail.com>
2025-11-14 11:57:51 +0530

    clean up import statements and maintain package structure

--- src/dynite/__init__.py
+++ src/dynite/__init__.py
@@ -2,8 +2,14 @@
 
 import logging
 
+from .client import Dynite
+
 __version__ = "0.1.0"
 
 # Set up a default logger for the package
 # Users of the library can configure logging as needed
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+# Expose the main Client class in the package namespace
+__all__ = ["Dynite"]
